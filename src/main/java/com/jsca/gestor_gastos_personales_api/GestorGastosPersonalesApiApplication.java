be943ca618cf50d31861a17d3b310d1297d71c3e
Sebas <elsebaslml@gmail.com>
2025-10-23 19:16:29 -0500

    Creacion del proyecto

---
+++ src/main/java/com/jsca/gestor_gastos_personales_api/GestorGastosPersonalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsca.gestor_gastos_personales_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorGastosPersonalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorGastosPersonalesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsca/gestor_gastos_personales_api/GestorGastosPersonalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsca.gestor_gastos_personales_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorGastosPersonalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
